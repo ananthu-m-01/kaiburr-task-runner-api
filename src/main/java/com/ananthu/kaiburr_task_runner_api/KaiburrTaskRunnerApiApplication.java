972c7e88c9b3eac65aec2c66569a4fdf5009c59a
Ananthu.M <ananthu.m.utr@gmail.com>
2025-10-14 17:53:03 +0530

    project initialized with java spring boot

---
+++ src/main/java/com/ananthu/kaiburr_task_runner_api/KaiburrTaskRunnerApiApplication.java
@@ -0,0 +1,13 @@
+package com.ananthu.kaiburr_task_runner_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaiburrTaskRunnerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaiburrTaskRunnerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ananthu/kaiburr_task_runner_api/KaiburrTaskRunnerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ananthu.kaiburr_task_runner_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaiburrTaskRunnerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
