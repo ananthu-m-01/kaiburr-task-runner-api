4029b29815bb6375c9d50491a1c1afd84cf5e1f0
Ananthu.M <ananthu.m.utr@gmail.com>
2025-10-19 18:01:01 +0530

    removed unwanted exceptions

--- src/main/java/com/ananthu/kaiburr_task_runner_api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ananthu/kaiburr_task_runner_api/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.ananthu.kaiburr_task_runner_api.exceptions;
 
 import com.ananthu.kaiburr_task_runner_api.exceptions.task.InvalidCommandException;
-import com.ananthu.kaiburr_task_runner_api.exceptions.task.TaskInvalidCredentialException;
 import com.ananthu.kaiburr_task_runner_api.exceptions.task.TaskNotFoundException;
 import com.ananthu.kaiburr_task_runner_api.exceptions.task_execution.TaskExecutionException;
 import org.springframework.http.HttpStatus;
@@ -16,19 +15,6 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
-
-//    handler for task invalid credential
-    @ExceptionHandler(TaskInvalidCredentialException.class)
-    public ResponseEntity<Map<String,Object>> handleInvalidCredentialException(TaskInvalidCredentialException taskInvalidCredentialException){
-        Map<String,Object> errorBody = new HashMap<>();
-        errorBody.put("timestamp", LocalDateTime.now());
-        errorBody.put("status", HttpStatus.UNAUTHORIZED.value());
-        errorBody.put("error", "invalid credential for task");
-        errorBody.put("message", taskInvalidCredentialException.getMessage());
-
-        return new ResponseEntity<>(errorBody,HttpStatus.INTERNAL_SERVER_ERROR);
-    }
 
 
 // handler for task not found exception
@@ -58,12 +44,12 @@ public class GlobalExceptionHandler {
 
     //    handler for task task execution
     @ExceptionHandler(TaskExecutionException.class)
-    public ResponseEntity<Map<String,Object>> handleTaskExecutionException(TaskInvalidCredentialException taskInvalidCredentialException){
+    public ResponseEntity<Map<String,Object>> handleTaskExecutionException(TaskExecutionException taskExecutionException){
         Map<String,Object> errorBody = new HashMap<>();
         errorBody.put("timestamp", LocalDateTime.now());
         errorBody.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
         errorBody.put("error", "task execution error");
-        errorBody.put("message", taskInvalidCredentialException.getMessage());
+        errorBody.put("message", taskExecutionException.getMessage());
 
         return new ResponseEntity<>(errorBody,HttpStatus.INTERNAL_SERVER_ERROR);
     }
--- src/main/java/com/ananthu/kaiburr_task_runner_api/exceptions/task/TaskInvalidCredentialException.java
+++
@@ -1,7 +0,0 @@
-package com.ananthu.kaiburr_task_runner_api.exceptions.task;
-
-public class TaskInvalidCredentialException extends RuntimeException {
-    public TaskInvalidCredentialException(String message) {
-        super(message);
-    }
-}
